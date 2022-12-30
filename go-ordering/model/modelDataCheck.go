228efcf9fd7a4fb28f127641db834fb46fc7818c
wslee <wslee220420@gmail.com>
2022-12-31 00:51:19 +0900

    uuid에서 -의 replace 삭제

--- go-ordering/model/modelDataCheck.go
+++ go-ordering/model/modelDataCheck.go
@@ -1,13 +1,19 @@
 package model
 
 import (
-	"strings"
-
 	"github.com/google/uuid"
 )
 
 func CreateUUID() string {
 	uuidValue := uuid.New()
-	uuid := strings.Replace(uuidValue.String(), "-", "", -1)
-	return uuid
+	/*
+		uuid 값에서 -를 제거하는 특별한 이유가 있을까요?
+		없다면 특별히 제거할 필요는 없다고 생각합니다.
+	*/
+	/*
+		수정내용
+		- 제거 삭제 주석처리
+	*/
+	//uuid := strings.Replace(uuidValue.String(), "-", "", -1)
+	return uuidValue.String()
 }
